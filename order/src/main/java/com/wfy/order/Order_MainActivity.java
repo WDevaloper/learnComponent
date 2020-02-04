92bc2e40482557f346b5c0c97c2440565991965e
wangfayouy <f_y_wang6@163.com>
2020-02-04 09:09:36 +0800

    update

--- order/src/main/java/com/wfy/order/Order_MainActivity.java
+++ order/src/main/java/com/wfy/order/Order_MainActivity.java
@@ -2,13 +2,13 @@ package com.wfy.order;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 
 import com.wfy.annotation.ARouter;
 
 @ARouter(path = "/app/Order_MainActivity")
 public class Order_MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
